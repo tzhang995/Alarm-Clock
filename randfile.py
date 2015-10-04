ec786d560ca396264622bb412842eae7610a7311
tzhang995 <tzhang995@gmail.com>
2015-10-04 12:15:54 -0400

    gets random file name

--- randfile.py
+++ randfile.py
@@ -1,6 +1,21 @@
 import os
+from random import randint
+b=randint(0,100)
 home = os.path.expanduser('~')
+filename="butts"
+ifdone=False
 print(home)
 for path, subdirs, files in os.walk(home):
     for name in files:
-        print os.path.join(path, name)
+        from random import randint
+        a=randint(0,100)
+        if a==b:
+            #filename=os.path.join(path, name)
+            filename=name
+            ifdone=True
+            break
+    if ifdone:
+        break
+
+print(filename)
+        
